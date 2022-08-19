066b12ba93be43892f972f7859682265cc7c7f86
Maggie Xian <111546244+meitongxian@users.noreply.github.com>
2022-08-19 22:15:03 +1200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//window.addEventListener("scroll", function(){
+    //var navBar = document.querySelector(".navbar");
+    //navBar.classList.toggle("sticky", window.scrollY > 0);
+//})
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".link-list");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger_active");
+    navMenu.classList.toggle("link-list_active");
+});
+
+document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click",() => {
+    hamburger.classList.remove("hamburger_active");
+    navMenu.classList.remove("link-list_active");
+}))
